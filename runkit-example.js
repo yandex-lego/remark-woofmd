6db559efab951fbd45e98d24df205ecae6bc53f7
Alexey Yaroshevich <qfox@yandex-team.ru>
2018-10-05 13:26:03 +0300

    docs: just runkit-example

---
+++ runkit-example.js
@@ -0,0 +1,38 @@
+const remark = require('remark');
+const { vis, remark: remarkWoofmd } = require('remark-woofmd');
+const processor = remark().data('settings', { commonmark: true, footnotes: true }).use(remarkWoofmd);
+const go = s => vis(processor.parse(s));
+
+go(`
+# Best addon for your pluphone
+
+Perfecto is real! Right, staff:johnston
+
+Text Before formatter block.
+%%(prettify align=center)
+Text inside
+formatter with //emphasis// and **bold**.
+%%
+Text just after the formatter block.
+
+<{ Big quote block here
+
+## With headings
+
+}>
+
+
+<{ Cut string **Bold here** and !!(green) Apple text !! too.
+    🍏🍎🍏🍎🍏🍎🍏🍏🍏🍏🍏🍒🍏🍒🍏🍒.
+
+    Yep.
+}>
+
+##+Rolled section
+
+This part is hidden for the first look
+
+## Usual heading
+
+Hey boys, it's the end!
+`);
